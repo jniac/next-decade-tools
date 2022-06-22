f92c25352147db6f69657fa72ea30b4023de4ef7
jniac <joseph.merdrignac@gmail.com>
2022-06-22 12:00:04 +0200

    (rfctr) Draggable -> Scroller

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import ReactMarkdown from 'react-markdown'
 import readme from 'tools/README.md'
 import { usePromise, useRefComplexEffects } from './some-utils/npm/react'
 import { handleFrame, handlePointer } from './some-utils/dom'
-import { Draggable2D } from './Draggable2D'
+import { Scroller2D } from './Scroller2D'
 import './App.css'
 
 const DragableDiv = () => {
   
   const ref = useRefComplexEffects<HTMLDivElement>(function* (div) {
     
-    const draggable = new Draggable2D()
+    const Scroller = new Scroller2D()
 
     const r1 = document.querySelector('.App > div')!.getBoundingClientRect()
     const r2 = div.getBoundingClientRect()
@@ -18,7 +18,7 @@ const DragableDiv = () => {
     const maxX = minX + (r1.width - r2.width)
     const maxY = minY + (r1.height - r2.height)
     
-    draggable.setMinMax(minX, minY, maxX, maxY)
+    Scroller.setMinMax(minX, minY, maxX, maxY)
 
     yield handlePointer(div, {
       onOver: () => {
@@ -29,23 +29,23 @@ const DragableDiv = () => {
       },
       onDown: () => {
         div.classList.add('dragged')
-        draggable.startDrag()
+        Scroller.startDrag()
       },
       onUp: () => {
         div.classList.remove('dragged')
-        draggable.stopDrag()
+        Scroller.stopDrag()
       },
       onDrag: info => {
-        draggable.drag(info.delta.x, info.delta.y)
+        Scroller.drag(info.delta.x, info.delta.y)
       },
     })
     
     yield handleFrame(() => {
-      if (isNaN(draggable.y) === false)
-      draggable.update()
+      if (isNaN(Scroller.y) === false)
+      Scroller.update()
       div.style.transform = `
-        translate(${draggable.x}px, ${draggable.y}px)
-        scale(${draggable.dragged ? 1.05 : 1})
+        translate(${Scroller.x}px, ${Scroller.y}px)
+        scale(${Scroller.dragged ? 1.05 : 1})
       `
     })
 
---
+++ src/Scroller2D.ts
@@ -0,0 +1,219 @@
+
+const limited = (x: number, limit: number) => (x * limit) / (x + limit);
+
+const limitedClamp = (x: number, limit: number, min: number, max: number) => {
+  if (x < min) {
+    return min - limited(min - x, limit);
+  }
+  if (x > max) {
+    return max + limited(x - max, limit);
+  }
+  return x;
+}
+
+const clamp = (x: number, min: number, max: number) => {
+  return x < min ? min : x > max ? max : x;
+}
+
+export type Scroller2DUpdateArg = Partial<{
+  deltaTime: number | 'guess'
+}>;
+
+/**
+ * Usage: 
+ * ```js
+ * const Scroller = new Scroller2D().zero().set({
+ *   minX: 0,
+ *   maxX: 1000,
+ *   marginX: 50,
+ * });
+ * 
+ * onFrame = () => {
+ *   Scroller.drag(dx, dy);
+ *   Scroller.update();
+ *   
+ *   myComponent.x = Scroller.x;
+ *   myComponent.y = Scroller.y;
+ * }
+ * ```
+ */
+export class Scroller2D {
+
+  private _time = Date.now();
+  private _timeOld = Date.now();
+  private _dragged = false;
+  private _deltaTime = 0;
+  private _x = 0;
+  private _y = 0;
+  private _xOld = 0;
+  private _yOld = 0;
+  private _dragDeltaX = 0;
+  private _dragDeltaY = 0;
+  private _clampedX = 0;
+  private _clampedY = 0;
+  
+  private _minX = -Infinity;
+  private _minY = -Infinity;
+  private _marginX = 20;
+  private _maxX = Infinity;
+  private _maxY = Infinity;
+  private _marginY = 20;
+  private _inertia = 0.001; // reduction by seconds: 0.001 means: only 0.1% velocity is 
+  
+  set({
+    x = this._x,
+    y = this._y,
+    minX = this._minX,
+    minY = this._minY,
+    marginX = this._marginX,
+    maxX = this._maxX,
+    maxY = this._maxY,
+    marginY = this._marginY,
+    inertia = this._inertia,
+  } = {}) {
+    this._minX = minX;
+    this._minY = minY;
+    this._marginX = marginX;
+    this._maxX = maxX;
+    this._maxY = maxY;
+    this._x = clamp(x, minX, maxX);
+    this._y = clamp(y, minY, maxY);
+    this._marginY = marginY;
+    this._inertia = inertia;
+    return this;
+  }
+
+  /**
+   * Reset to zero min, max, margin & position values.  
+   * By default there are infinite bounds (min, max).  
+   * So one requires to use `set()` after, eg: 
+   * ```
+   * Scroller.zero().set({ maxX: 200 })
+   * ```
+   */
+  zero() {
+    this._minX = 0;
+    this._maxX = 0;
+    this._minY = 0;
+    this._maxY = 0;
+    this._marginX = 0;
+    this._marginY = 0;
+    this._x = 0;
+    this._y = 0;
+    return this;
+  }
+
+  update({
+    deltaTime = 'guess',
+  }: Scroller2DUpdateArg = {}) {
+
+    this._timeOld = this._time;
+    this._xOld = this._x;
+    this._yOld = this._y;
+
+    if (deltaTime === 'guess') {
+      this._time = Date.now() / 1000;
+      this._deltaTime = this._time - this._timeOld;
+    }
+    else {
+      this._deltaTime = deltaTime;
+      this._time += deltaTime;
+    }
+
+    if (this._deltaTime > 0) {
+      this._x += this._dragDeltaX;
+      this._y += this._dragDeltaY;
+
+      if (this._dragged) {
+        this._clampedX = limitedClamp(this._x, this._marginX, this._minX, this._maxX);
+        this._clampedY = limitedClamp(this._y, this._marginY, this._minY, this._maxY);
+      } 
+      else {
+        if (this._clampedX < this._minX) {
+          this._clampedX += (this._minX - this._clampedX) * .2;
+          this._x = this._clampedX;
+        }
+        else if (this._clampedX > this._maxX) {
+          this._clampedX += (this._maxX - this._clampedX) * .2;
+          this._x = this._clampedX;
+        }
+        else {
+          this._clampedX = this._x;
+        }
+
+        if (this._clampedY < this._minY) {
+          this._clampedY += (this._minY - this._clampedY) * .2;
+          this._y = this._clampedY;
+        }
+        else if (this._clampedY > this._maxY) {
+          this._clampedY += (this._maxY - this._clampedY) * .2;
+          this._y = this._clampedY;
+        }
+        else {
+          this._clampedY = this._y;
+        }
+      }
+
+      if (this._dragged === false) {
+        const di = this._inertia ** this._deltaTime;
+        this._dragDeltaX = this._dragDeltaX * di;
+        this._dragDeltaY = this._dragDeltaY * di;
+      }
+    }
+
+    return this;
+  }
+
+  setMinMaxX(minX: number, maxX: number) {
+    this._minX = minX;
+    this._maxX = maxX;
+    return this;
+  }
+
+  setMinMaxY(minY: number, maxY: number) {
+    this._minY = minY;
+    this._maxY = maxY;
+    return this;
+  }
+
+  setMin(minX: number, minY: number) {
+    this._minX = minX;
+    this._minY = minY;
+    return this;
+  }
+
+  setMax(maxX: number, maxY: number) {
+    this._maxX = maxX;
+    this._maxY = maxY;
+    return this;
+  }
+
+  setMinMax(minX: number, minY: number, maxX: number, maxY: number) {
+    return this.setMin(minX, minY).setMax(maxX, maxY);
+  }
+
+  startDrag() {
+    this._dragged = true;
+    this._dragDeltaX = 0;
+    this._dragDeltaY = 0;
+    return this;
+  }
+
+  stopDrag() {
+    this._dragged = false;
+    return this;
+  }
+
+  drag(dragX: number, dragY: number) {
+    this._dragDeltaX = dragX;
+    this._dragDeltaY = dragY;
+    return this;
+  }
+
+  // exposition
+  get dragged() { return this._dragged; }
+  get x() { return this._clampedX; }
+  get y() { return this._clampedY; }
+  get dx() { return this._x - this._xOld; }
+  get dy() { return this._y - this._yOld; }
+}
